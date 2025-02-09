65c9d82d6776e0cc23c06b065e50850d4fe49836
tranthang98 <tranthang98>
2025-02-09 16:31:34 +0700

    setup mongoose, nestjs/mongoose, @nestjs/config (to get env vars)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService
+  ) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get() // route /=> api (restful)
+  @Render("home") //home.ejs
+  getHello() {
+    //port from .env
+    console.log(">> check port = ", this.configService.get<string>("PORT"));
+
+    const message1 = this.appService.getHello();
+    
+    return {
+      message: message1,
+    }
+    // return this.appService.getHello();
   }
+
+  @Get("abc")
+  getHello1(): string {
+    return "hello";
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+  app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();

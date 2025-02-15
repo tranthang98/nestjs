0bea86c2dfc2eca29be95bcc9b87e8834c8875ac
Thang Tran <tranthang98hust@gmail.com>
2025-02-15 18:56:39 +0700

    feat(auth): update cors config

--- src/main.ts
+++ src/main.ts
@@ -31,9 +31,10 @@ async function bootstrap() {
   //config cors
   app.enableCors(
     {
-      "origin": "http://localhost:3000",
+      "origin": true,
       "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
       "preflightContinue": false,
+      credentials: true
     }
   );
 

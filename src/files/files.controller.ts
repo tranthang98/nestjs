f884b841265d4ecfca90074c97e36f24a8856edc
tranthang98 <tranthang98>
2025-02-16 01:14:38 +0700

    feat(file): create destination Upload File

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -15,7 +15,7 @@ export class FilesController {
   uploadFile(@UploadedFile(
     new ParseFilePipeBuilder()
       .addFileTypeValidator({
-        fileType: /^(jpg|jpeg|png|image\/png|gif|txt|pdf|application\/pdf|doc|docx|text\/plain)$/i,
+        fileType: /^(jpg|image\/jpeg|jpeg|png|image\/png|gif|txt|pdf|application\/pdf|doc|docx|text\/plain)$/i,
       })
       .addMaxSizeValidator({
         maxSize: 1024 * 1024
@@ -24,7 +24,9 @@ export class FilesController {
         errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
       }),
   ) file: Express.Multer.File) {
-    console.log(file);
+    return {
+      fileName: file.filename
+    }
   }
 
   @Get()

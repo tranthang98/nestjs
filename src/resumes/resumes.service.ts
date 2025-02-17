20248cb9fcc47663f7eaba3a3511e5aef53b98ac
tranthang98 <tranthang98>
2025-02-18 00:54:27 +0700

    feat(resume): api fetch resume - by - user => populate companyId, jobId

--- src/resumes/resumes.service.ts
+++ src/resumes/resumes.service.ts
@@ -78,7 +78,17 @@ export class ResumesService {
   async findByUsers(user: IUser) {
     return await this.resumeModel.find({
       userId: user._id
-    });
+    }).sort("-createdAt")
+      .populate([
+        {
+          path: "companyId",
+          select: { name: 1 }
+        },
+        {
+          path: "jobId",
+          select: { name: 1 }
+        }
+      ])
   }
 
   async findOne(id: string) {

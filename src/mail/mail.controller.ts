63403403f8ca209f8cb4192d0e8a3c24bc00213c
Thang Tran <tranthang98hust@gmail.com>
2025-03-02 00:43:58 +0700

    feat(mail): fetch subcribers by skills

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,12 +1,24 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Controller, Get } from '@nestjs/common';
 import { Public, ResponseMessage } from 'src/decorator/customize';
+import { SubscribersService } from 'src/subscribers/subscribers.service';
 import { MailService } from './mail.service';
-import { MailerService } from '@nestjs-modules/mailer';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { SubscriberDocument } from 'src/subscribers/schemas/subscriber.schema';
+import { Job, JobDocument } from 'src/jobs/schemas/job.schemas';
+import { InjectModel } from '@nestjs/mongoose';
+import { Subscriber } from 'rxjs';
 
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService,
-    private mailerService: MailerService
+    private mailerService: MailerService,
+
+    @InjectModel(Subscriber.name)
+    private subscriberModel: SoftDeleteModel<SubscriberDocument>,
+
+    @InjectModel(Job.name)
+    private jobModel: SoftDeleteModel<JobDocument>
   ) {
 
   }
@@ -15,17 +27,32 @@ export class MailController {
   @Public()
   @ResponseMessage("Test email")
   async handleTestEmail() {
-    await this.mailerService.sendMail({
-      to: "someone@gmail.com",
-      from: '"Support Team" <support@example.com>',
-      subject: 'Welcome to Nice App! Confirm your Email',
-      template: 'new-job',
-      context: {
-        receiver: "Thang",
-        job: "IT",
-        url: "XXXXXXXXXXXXXXXXXXXXX"
+    const subscribers = await this.subscriberModel.find({});
+    for (const subs of subscribers) {
+      const subsSkills = subs.skills;
+      const jobWithMatchingSkills = await this.jobModel.find({ skills: { $in: subsSkills } });
+      if (jobWithMatchingSkills?.length) {
+        const jobs = jobWithMatchingSkills.map(item => {
+          return {
+            name: item.name,
+            company: item.company.name,
+            salary: `${item.salary}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',') + " đ",
+            skills: item.skills
+          }
+        })
+        await this.mailerService.sendMail({
+          to: subs.email,
+          from: '"Support Team" <support@example.com>',
+          subject: 'Welcome to Nice App! Confirm your Email',
+          template: 'new-job',
+          context: {
+            receiver: subs.name,
+            jobs: jobs
+          }
+        });
       }
-    });
+    }
+
   }
 
 }
--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -5,6 +5,10 @@ import { ConfigService } from '@nestjs/config';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { join } from 'path';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Subscriber } from 'rxjs';
+import { SubscriberSchema } from 'src/subscribers/schemas/subscriber.schema';
+import { Job, JobSchema } from 'src/jobs/schemas/job.schemas';
 
 @Module({
   imports: [
@@ -29,6 +33,10 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
       }),
       inject: [ConfigService],
     }),
+    MongooseModule.forFeature([
+      { name: Subscriber.name, schema: SubscriberSchema },
+    ]),
+    MongooseModule.forFeature([{ name: Job.name, schema: JobSchema }])
   ],
   controllers: [MailController],
   providers: [MailService]
